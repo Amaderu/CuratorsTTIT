69c7a8439775e957b8c41f4f88c64949bea99191
unknown <mad@LAB403-K4.ttit.local>
2022-02-15 16:20:36 +0700

    added files

--- app/src/main/java/com/example/curatorsttit/MainActivity.java
+++ app/src/main/java/com/example/curatorsttit/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.curatorsttit;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActionBar actionBar;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        // Get ActionBar
+        //Activity.
+        //actionBar = getSupportActionBar();
+        // Set below attributes to add logo in ActionBar.
+        //actionBar.setDisplayShowHomeEnabled(true);
+        //actionBar.setTitle("");
+        //toolbar = (Toolbar) Toolbar.inflate(getApplicationContext(),R.layout.toolbar,findViewById(R.id.mainActivity));
+        //setSupportActionBar(toolbar);
+        //actionBar.setDisplayUseLogoEnabled(true);
+        //actionBar.setLogo(R.drawable.ic_bird);
+        //actionBar.setTitle("Dev2Qa.com");
+
+
+
     }
 }
--- app/src/main/java/com/example/curatorsttit/MainFragment.java
+++ app/src/main/java/com/example/curatorsttit/MainFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link mainFragment#newInstance} factory method to
+ * Use the {@link MainFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class mainFragment extends Fragment {
+public class MainFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class mainFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public mainFragment() {
+    public MainFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class mainFragment extends Fragment {
      * @return A new instance of fragment mainFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static mainFragment newInstance(String param1, String param2) {
-        mainFragment fragment = new mainFragment();
+    public static MainFragment newInstance(String param1, String param2) {
+        MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -53,12 +53,20 @@ public class mainFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        /*Spinner spinner = (Spinner) view.findViewById(R.id.spinner_groups);
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(getContext(), R.array.groups,R.layout.cus_spinn);
+        adapter.setDropDownViewResource(R.layout.cus_drop_spinn);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);*/
+        return view;
     }
 }
